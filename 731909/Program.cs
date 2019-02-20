be6d3e90582e91377e673e8ca32d003dfe37f500
visualstudio <visualstudio@122-PC20>
2019-02-20 11:56:41 -0500

    baby

--- 731909/Program.cs
+++ 731909/Program.cs
@@ -10,6 +10,101 @@ namespace _731909
     {
         static void Main(string[] args)
         {
+            //new inder().kharoud();
+            var a = new CountrySide();
+            a.Run();
+
+
+            Console.WriteLine(UsingRecursion.CalculateRecursively(3, 4));
+
+
+        }
+
+        class inder
+        {
+            static int j = 0;
+
+            public void  kharoud()
+            {
+                //let's write a trace message
+               
+                Console.WriteLine(inder.j++);
+                int Sum = 0;
+                Sum += 1;
+                kharoud();
+
+            }
+        }
+
+
+        class UsingRecursion
+        {
+            // Create an application which 
+            // calculates the sum of all the numbers from n to m recursively:
+
+            public static int CalculateRecursively(int n, int m)
+            {
+                int sum = n;
+
+                if (n < m)
+                {
+                    n++;
+                    return sum += CalculateRecursively(n, m);
+                }
+                return sum;
+            }
+
+
+        }
+
+        class CountrySide
+        {
+            // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+            Village Maeland;
+            Village Helmholtz;
+            Village Alst;
+            Village Wessig;
+            Village Badden;
+            Village Uster;
+            Village Schvenig;
+
+            public void Run()
+            {
+                Village Alst = new Village("Alst", false);
+                Village Schvenig = new Village(" Schvenig ", false);
+                Village Wessig  = new Village("Wessig", true);
+                Alst.west = Schvenig;
+                Alst.east = Wessig;
+                Alst.distanceToWestVillage = 14;
+                Alst.distanceToEastVillage = 19;
+
+                
+            }
+            public void travelVillages()
+            {
+
+            }
+        }
+
+
+            class Village
+            {
+                public Village(string _villageName, bool _isAHere)
+                {
+                    isAstrildgeHere = _isAHere;
+                    VillageName = _villageName;
+                }
+
+                public Village west;
+                public Village east;
+                public string VillageName;
+                public int distanceToNextVillage;
+            public int distanceToWestVillage;
+            public int distanceToEastVillage;
+            public int distanceToPreviousVillage;
+                public bool isAstrildgeHere;
+            }
+
         }
     }
 }
